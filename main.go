89930b77f538382889650232471ff31d05154a6c
Hugo <hugovk@users.noreply.github.com>
2017-12-01 17:17:16 +0200

    Rip out facefinder

--- main.go
+++ main.go
@@ -9,12 +9,9 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/zikes/chrisify/facefinder"
-
 	"github.com/disintegration/imaging"
 )
 
-var haarCascade = flag.String("haar", "haarcascade_frontalface_alt.xml", "The location of the Haar Cascade XML configuration to be provided to OpenCV.")
 var facesDir = flag.String("faces", "faces", "The directory to search for faces.")
 
 func main() {
@@ -42,8 +39,6 @@ func main() {
 
 	file := flag.Arg(0)
 
-	finder := facefinder.NewFinder(*haarCascade)
-
 	baseImage := loadImage(file)
 
 	faces := finder.Detect(baseImage)
@@ -52,40 +47,20 @@ func main() {
 
 	canvas := canvasFromImage(baseImage)
 
-	for _, face := range faces {
-		rect := rectMargin(30.0, face)
-
-		newFace := chrisFaces.Random()
-		if newFace == nil {
-			panic("nil face")
-		}
-		chrisFace := imaging.Fit(newFace, rect.Dx(), rect.Dy(), imaging.Lanczos)
-
-		draw.Draw(
-			canvas,
-			rect,
-			chrisFace,
-			bounds.Min,
-			draw.Over,
-		)
-	}
-
-	if len(faces) == 0 {
-		face := imaging.Resize(
-			chrisFaces[0],
-			bounds.Dx()/3,
-			0,
-			imaging.Lanczos,
-		)
-		face_bounds := face.Bounds()
-		draw.Draw(
-			canvas,
-			bounds,
-			face,
-			bounds.Min.Add(image.Pt(-bounds.Max.X/2+face_bounds.Max.X/2, -bounds.Max.Y+int(float64(face_bounds.Max.Y)/1.9))),
-			draw.Over,
-		)
-	}
+	face := imaging.Resize(
+		chrisFaces[0],
+		bounds.Dx()/3,
+		0,
+		imaging.Lanczos,
+	)
+	face_bounds := face.Bounds()
+	draw.Draw(
+		canvas,
+		bounds,
+		face,
+		bounds.Min.Add(image.Pt(-bounds.Max.X/2+face_bounds.Max.X/2, -bounds.Max.Y+int(float64(face_bounds.Max.Y)/1.9))),
+		draw.Over,
+	)
 
 	jpeg.Encode(os.Stdout, canvas, &jpeg.Options{jpeg.DefaultQuality})
 }
